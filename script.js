7a75035994b7ee0999645bec60ee21d9e98282c9
Arunss1729 <91784368+Arunss1729@users.noreply.github.com>
2022-01-19 11:57:23 +0530

    mycalculator1

---
+++ script.js
@@ -0,0 +1,14 @@
+function bclick(val)
+{ 
+    console.log(val)
+    document.getElementById('screen').value+=val
+}
+function bclear(){
+    document.getElementById('screen').value=''
+}
+function bresult(){
+    var text=document.getElementById('screen').value
+    var result=eval(text)
+    document.getElementById('screen').value=result
+
+}
